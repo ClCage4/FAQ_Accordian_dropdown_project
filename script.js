32aaa8d03991265a5d0a283461977eeafe9b09a5
ClCage4 <112894479+ClCage4@users.noreply.github.com>
2022-10-24 13:01:10 -0500

    added JS functions

---
+++ script.js
@@ -0,0 +1,16 @@
+const checkbox = document.getElementsByName("checkbox");
+const accordion = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < checkbox.length; i++) {
+  const element = checkbox[i];
+  element.addEventListener("change", function () {
+    if (accordion[i].classList.contains("active")) {
+      accordion[i].classList.remove("active");
+    } else {
+      for (let j = 0; j < accordion.length; j++) {
+        accordion[j].classList.remove("active");
+      }
+      accordion[i].classList.add("active");
+    }
+  });
+}
